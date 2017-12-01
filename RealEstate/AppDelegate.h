bc8284e4eca2bcab37a48a1844600032ac9491de
harikrishna patel <nimisha@softqube.co.in>
2017-12-01 10:39:01 +0530

    Initial Commit

---
+++ RealEstate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealEstate
+//
+//  Created by harikrishna patel on 01/12/17.
+//  Copyright © 2017 Softqube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealEstate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealEstate
+//
+//  Created by harikrishna patel on 01/12/17.
+//  Copyright © 2017 Softqube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
